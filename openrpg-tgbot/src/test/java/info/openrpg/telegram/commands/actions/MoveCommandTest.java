ff27a9d61cb689163557b38cf2af3bd954079af6
Dark Casual <dark.poison.casual@gmail.com>
2017-05-20 12:21:53 +0300

    [BF] fix test

--- openrpg-tgbot/src/test/java/info/openrpg/telegram/commands/actions/MoveCommandTest.java
+++ openrpg-tgbot/src/test/java/info/openrpg/telegram/commands/actions/MoveCommandTest.java
@@ -4,9 +4,15 @@ import com.google.common.base.Joiner;
 import info.openrpg.database.models.Player;
 import info.openrpg.database.repositories.PlayerDao;
 import info.openrpg.gameserver.WorldInstance;
+import info.openrpg.gameserver.enums.GameClass;
+import info.openrpg.gameserver.enums.MoveDirections;
+import info.openrpg.gameserver.enums.Race;
+import info.openrpg.gameserver.model.actors.PlayerStats;
+import info.openrpg.gameserver.model.world.Location;
 import info.openrpg.image.processing.RequestSender;
 import info.openrpg.telegram.commands.Message;
 import info.openrpg.telegram.constants.Command;
+import info.openrpg.telegram.constants.Direction;
 import info.openrpg.telegram.io.InlineButton;
 import info.openrpg.telegram.io.InputMessage;
 import info.openrpg.telegram.io.MessageWrapper;
@@ -22,6 +28,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -39,7 +46,7 @@ public class MoveCommandTest {
     public void setUp() throws Exception {
         PlayerDao playerDao = mock(PlayerDao.class);
         RequestSender requestSender = mock(RequestSender.class);
-        when(requestSender.sendMove(CHAT_ID, 1, 1)).thenReturn(Optional.of(STREAM));
+        when(requestSender.createImage(any())).thenReturn(Optional.of(STREAM));
 
         Player player = Player.builder()
                 .firstName("Dark")
@@ -49,6 +56,10 @@ public class MoveCommandTest {
                 .build();
         when(playerDao.findPlayerByUsername(USER_NAME)).thenReturn(Optional.of(player));
         worldInstance = new WorldInstance();
+        info.openrpg.gameserver.model.actors.Player serverPlayer = mock(info.openrpg.gameserver.model.actors.Player.class);
+        when(serverPlayer.getPlayerId()).thenReturn(CHAT_ID.intValue());
+        when(serverPlayer.getCurLocation()).thenReturn(new Location(1, 1, 1, 1));
+        worldInstance.addPlayer(serverPlayer);
         command = new MoveCommand(playerDao, requestSender);
     }
 
@@ -70,20 +81,22 @@ public class MoveCommandTest {
     @Test
     public void testFullCommandExecute() throws Exception {
         User user = mock(User.class);
+        when(user.getId()).thenReturn(CHAT_ID.intValue());
         when(user.getUserName()).thenReturn(USER_NAME);
         InputMessage inputMessage = new InputMessage(
-                JOINER.join(Command.MOVE, 1, 1),
+                JOINER.join(Command.MOVE, MoveDirections.SOUTH.name()),
                 CHAT_ID,
                 user
         );
         List<MessageWrapper> wrappers = command.execute(inputMessage, worldInstance);
 
         Assert.assertFalse(wrappers.isEmpty());
-        SendPhoto photo = wrappers.get(0).getPhoto();
+        MessageWrapper.PhotoToMultipleUsers photo = wrappers.get(0).getPhotoToMultipleUsers();
         Assert.assertNotNull(photo);
-        Assert.assertEquals(photo.getPhotoName(), PHOTO_NAME);
-        Assert.assertEquals(photo.getChatId(), String.valueOf(CHAT_ID));
-        Assert.assertEquals(photo.getReplyMarkup(), InlineButton.moveButtonList());
+        Assert.assertEquals(photo.getActualPhoto().getPhotoName(), PHOTO_NAME);
+        Assert.assertEquals(photo.getActualPhoto().getChatId(), String.valueOf(CHAT_ID));
+        Assert.assertEquals(photo.getActualPhoto().getReplyMarkup(), InlineButton.moveButtonList());
+        Assert.assertTrue(photo.getReceivers().isEmpty());
     }
 
     @Test
